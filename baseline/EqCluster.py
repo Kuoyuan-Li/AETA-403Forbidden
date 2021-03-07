58bcf592ecd2f3046d01af475c3da370c2df6792
李廓园 <likuoyuan@rikuruwas-MacBook-Pro.local>
2021-03-07 23:29:47 +1100

    basic clustering

--- baseline/EqCluster.py
+++ baseline/EqCluster.py
@@ -1,6 +1,9 @@
 import os
 import re
-import glob
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
 from config import *
 
 
@@ -9,7 +12,7 @@ def concat_eq_data ():
     re_eq = re.compile(r'EC_(\d){8}-(\d){8}.csv')  # re to find all earthquake data files
     eq_fileName_list = [f for f in fileName_list if re.search(re_eq, f)]  # find eq data files
     eq_fileName_list = [Data_Folder_Path + eq_file for eq_file in eq_fileName_list]  # add path
-    eqlst = open(Data_Folder_Path+"eqlst.csv","w")  # merge all eq data to eqlst.csv
+    eqlst = open(Eq_list_path,"w")  # merge all eq data to eqlst.csv
     if (len(eq_fileName_list))== 0: # no eq data file being found
         eqlst.write(eq_header)
         eqlst.close()
@@ -28,5 +31,17 @@ def concat_eq_data ():
                 eqlst.write(line)
             eq_data.close()
         eqlst.close()
+
+def eq_cluster_Kmeans():
+    eq_data = pd.read_csv(Eq_list_path)
+    eq_data = eq_data[['Longitude','Latitude','Magnitude']]
+    eq_loc = eq_data[['Longitude','Latitude']].to_numpy()  # use location to cluster earthquakes
+    clusters = KMeans(n_clusters=8).fit_predict(eq_loc)  # no weight cluster
+    # visualizing the results
+    u_labels = np.unique(clusters)
+    for i in u_labels:
+        plt.scatter(eq_loc[clusters == i, 0], eq_loc[clusters == i, 1], label=i)
+    plt.legend()
+    plt.show()
 
 
